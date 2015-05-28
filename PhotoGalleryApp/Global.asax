4f8dca7d052566c7c69074b8b1545aa691416caf
codingonHP <vishal_24_anand@hotmail.com>
2015-05-28 15:56:31 +0530

    initial Commit

---
+++ PhotoGalleryApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhotoGalleryApp.MvcApplication" Language="C#" %>
---
+++ PhotoGalleryApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhotoGalleryApp.Startup))]
+namespace PhotoGalleryApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
